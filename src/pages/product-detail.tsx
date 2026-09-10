299e74dccc144f129511ecb84bcab2cd3c7b0ff5
Josh Dickson <jodicksonjoshua@gmail.com>
2026-09-10 18:09:12 +0100

    feat: add 4th sub image to 10ft cargo basket gallery

--- src/pages/product-detail.tsx
+++ src/pages/product-detail.tsx
@@ -27,6 +27,7 @@ const PRODUCT_GALLERY: Record<string, string[]> = {
     '/images/cargo-basket-10ft-1.jpg',
     '/images/cargo-basket-10ft-2.jpg',
     '/images/cargo-basket-10ft-3.jpg',
+    '/images/cargo-basket-10ft-4.jpg',
   ],
   '20ft-cargo-basket': [
     '/images/cargo-basket-20ft-1.jpg',
